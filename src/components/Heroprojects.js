0b9155d272da0eb61d022e98ac9c84281deeebef
Rukiye Aydın <rukiyeaydinlive@gmail.com>
2023-08-10 12:33:43 +0300

    projects section changes

--- src/components/Heroprojects.js
+++ src/components/Heroprojects.js
@@ -9,13 +9,13 @@ const Heroprojects = () => {
   return (
     <Parallax blur={5} bgImage={Code} strength={500} className='foto '>
       <div className="prlx flex flex-wrap m-2"> 
-        <Link to="advanced" spy={true} smooth={true} offset={-70} duration={1000} className="bir w-72 bg-white flex-col items-center content-center text-center font-black p-1 mr-2 rounded-md cursor-pointer">
-          <p className='p-2 text-xl'>Advanced Projects</p>
+        <Link to="advanced" spy={true} smooth={true} offset={-70} duration={1000} className="bir w-72 bg-white flex-col items-center content-center text-center font-black p-1 m-2 rounded-md cursor-pointer">
+          <p class="p-2 text-xl" id="advanced-projects">Advanced Projects</p>
           <div className="flex items-center justify-center text-2xl"> 
             <AiOutlineArrowDown />
           </div>
         </Link>
-        <Link to="forbeginners" spy={true} smooth={true} offset={-70} duration={1800} className="iki w-72 bg-white flex-col items-center content-center text-center font-black p-1 rounded-md cursor-pointer">
+        <Link to="forbeginners" spy={true} smooth={true} offset={-70} duration={1800} className="iki w-72 bg-white flex-col items-center content-center text-center font-black p-1 m-2 rounded-md cursor-pointer">
           <p className='p-2 font-bold text-xl'>Projects For Beginners</p>
           <div className="flex items-center justify-center text-2xl">
             <AiOutlineArrowDown />
