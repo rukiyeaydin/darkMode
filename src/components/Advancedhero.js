b141a295f7084032da754437ea35283dd97b229e
Rukiye Aydın <rukiyeaydinlive@gmail.com>
2023-08-10 13:02:17 +0300

    responsive projects

--- src/components/Advancedhero.js
+++ src/components/Advancedhero.js
@@ -9,11 +9,11 @@ const Advancedhero = () => {
   return (
     <Parallax blur={5} bgImage={Code} strength={500} className='advancedhero'> 
         <div className="my-20">
-            <div className="prlx2 flex gap-3 flex-wrap m-2">
-                <div className="p-3 w-auto bg-gray-100 rounded-sm">
-                    <div className="w-auto h-64"><img class="w-64 h-64 rounded-md" src={fashion}/></div>
+            <div className="prlx2 flex gap-24 flex-wrap m-2">
+                <div className="p-3 w-auto bg-gray-100 rounded-2xl">
+                    <div className="w-auto h-80"><img class="w-80 h-80 rounded-3xl" src={fashion}/></div>
                     <div className="">
-                        <h3 class="text-lg mt-1 font-bold">E-commerce</h3>
+                        <h3 class="text-lg mt-1 font-bold max-w-xs">E-commerce</h3>
                         <div className="flex justify-between items-center pt-3 pb-2">
                             <a href="#" className="px-4 py-2 bg-blue-600 hover:bg-blue-400 text-center text-sm text-white rounded duration-300">Live Demo</a>
                             <a className="text-black cursor-pointer text-3xl hover:text-blue-600" href='https://github.com/rukiyeaydin/e-commerce'>
--- src/components/Forbeginnershero.js
+++ src/components/Forbeginnershero.js
@@ -17,11 +17,11 @@ const Forbeginnershero = () => {
     <div>
     <Parallax blur={5} bgImage={Code} strength={500} className='advancedhero'> 
         <div className="my-20">
-            <div className="prlx2 flex gap-3 flex-wrap m-2">
-                <div className="p-3 w-auto bg-gray-100 rounded-sm">
-                    <div className="w-auto h-64"><img class="w-64 h-64 rounded-md" src={bodymass}/></div>
+            <div className="prlx2 flex gap-24 flex-wrap m-2">
+                <div className="p-3 w-auto bg-gray-100 rounded-2xl">
+                    <div className="w-auto h-80"><img class="w-80 h-80 rounded-3xl" src={bodymass}/></div>
                     <div className="">
-                        <h3 class="text-lg mt-1 font-bold">Body Mass Index Calculator smthsıauhıusha absuıhua</h3>
+                        <h3 class="text-lg mt-1 font-bold max-w-xs">Body Mass Index Calculator</h3>
                         <div className="flex justify-between items-center pt-3 pb-2">
                             <a href="#" className="px-4 py-2 bg-blue-600 hover:bg-blue-400 text-center text-sm text-white rounded duration-300">Live Demo</a>
                             <a className="text-black cursor-pointer text-3xl hover:text-blue-600" href='https://github.com/rukiyeaydin/vucut_kitle_indeksi'>
@@ -30,10 +30,10 @@ const Forbeginnershero = () => {
                         </div>
                     </div>
                 </div>
-                <div className="p-3 w-auto bg-gray-100 rounded-sm">
-                    <div className="w-auto h-64"><img class="w-64 h-64 rounded-md" src={calculator}/></div>
+                <div className="p-3 w-auto bg-gray-100 rounded-2xl">
+                    <div className="w-auto h-80"><img class="w-80 h-80 rounded-3xl" src={calculator}/></div>
                     <div className="">
-                        <h3 class="text-lg mt-1 font-bold">Calculator</h3>
+                        <h3 class="text-lg mt-1 font-bold max-w-xs">Calculator</h3>
                         <div className="flex justify-between items-center pt-3 pb-2">
                             <a href="#" className="px-4 py-2 bg-blue-600 hover:bg-blue-400 text-center text-sm text-white rounded duration-300">Live Demo</a>
                             <a className="text-black cursor-pointer text-3xl hover:text-blue-600" href='https://github.com/rukiyeaydin/calculator'>
@@ -42,10 +42,10 @@ const Forbeginnershero = () => {
                         </div>
                     </div>
                 </div>
-                <div className="p-3 w-auto bg-gray-100 rounded-sm">
-                    <div className="w-auto h-64"><img class="w-64 h-64 rounded-md" src={digitalclock}/></div>
+                <div className="p-3 w-auto bg-gray-100 rounded-2xl">
+                    <div className="w-auto h-80"><img class="w-80 h-80 rounded-3xl" src={digitalclock}/></div>
                     <div className="">
-                        <h3 class="text-lg mt-1 font-bold">Digital Clock</h3>
+                        <h3 class="text-lg mt-1 font-bold max-w-xs">Digital Clock</h3>
                         <div className="flex justify-between items-center pt-3 pb-2">
                             <a href="#" className="px-4 py-2 bg-blue-600 hover:bg-blue-400 text-center text-sm text-white rounded duration-300">Live Demo</a>
                             <a className="text-black cursor-pointer text-3xl hover:text-blue-600" href='https://github.com/rukiyeaydin/digital_clock'>
@@ -54,10 +54,10 @@ const Forbeginnershero = () => {
                         </div>
                     </div>
                 </div>
-                <div className="p-3 w-auto bg-gray-100 rounded-sm">
-                    <div className="w-auto h-64"><img class="w-64 h-64 rounded-md" src={guessnumber}/></div>
+                <div className="p-3 w-auto bg-gray-100 rounded-2xl">
+                    <div className="w-auto h-80"><img class="w-80 h-80 rounded-3xl" src={guessnumber}/></div>
                     <div className="">
-                        <h3 class="text-lg mt-1 font-bold">Number Guess Game</h3>
+                        <h3 class="text-lg mt-1 font-bold max-w-xs">Number Guess Game</h3>
                         <div className="flex justify-between items-center pt-3 pb-2">
                             <a href="#" className="px-4 py-2 bg-blue-600 hover:bg-blue-400 text-center text-sm text-white rounded duration-300">Live Demo</a>
                             <a className="text-black cursor-pointer text-3xl hover:text-blue-600" href='https://github.com/rukiyeaydin/guess_number'>
@@ -66,10 +66,10 @@ const Forbeginnershero = () => {
                         </div>
                     </div>
                 </div>
-                <div className="p-3 w-auto bg-gray-100 rounded-sm">
-                    <div className="w-auto h-64"><img class="w-64 h-64 rounded-md" src={password}/></div>
+                <div className="p-3 w-auto bg-gray-100 rounded-2xl">
+                    <div className="w-auto h-80"><img class="w-80 h-80 rounded-3xl" src={password}/></div>
                     <div className="">
-                        <h3 class="text-lg mt-1 font-bold">Password Strength Checker</h3>
+                        <h3 class="text-lg mt-1 font-bold max-w-xs">Password Strength Checker</h3>
                         <div className="flex justify-between items-center pt-3 pb-2">
                             <a href="#" className="px-4 py-2 bg-blue-600 hover:bg-blue-400 text-center text-sm text-white rounded duration-300">Live Demo</a>
                             <a className="text-black cursor-pointer text-3xl hover:text-blue-600" href='https://github.com/rukiyeaydin/password_strength_check'>
@@ -78,10 +78,10 @@ const Forbeginnershero = () => {
                         </div>
                     </div>
                 </div>
-                <div className="p-3 w-auto bg-gray-100 rounded-sm">
-                    <div className="w-auto h-64"><img class="w-64 h-64 rounded-md" src={pixelart}/></div>
+                <div className="p-3 w-auto bg-gray-100 rounded-2xl">
+                    <div className="w-auto h-80"><img class="w-80 h-80 rounded-3xl" src={pixelart}/></div>
                     <div className="">
-                        <h3 class="text-lg mt-1 font-bold">Pixel Art</h3>
+                        <h3 class="text-lg mt-1 font-bold max-w-xs">Pixel Art</h3>
                         <div className="flex justify-between items-center pt-3 pb-2">
                             <a href="#" className="px-4 py-2 bg-blue-600 hover:bg-blue-400 text-center text-sm text-white rounded duration-300">Live Demo</a>
                             <a className="text-black cursor-pointer text-3xl hover:text-blue-600" href='https://github.com/rukiyeaydin/pixelArt'>
@@ -90,10 +90,10 @@ const Forbeginnershero = () => {
                         </div>
                     </div>
                 </div>
-                <div className="p-3 w-auto bg-gray-100 rounded-sm">
-                    <div className="w-auto h-64"><img class="w-64 h-64 rounded-md" src={rgb}/></div>
+                <div className="p-3 w-auto bg-gray-100 rounded-2xl">
+                    <div className="w-auto h-80"><img class="w-80 h-80 rounded-3xl" src={rgb}/></div>
                     <div className="">
-                        <h3 class="text-lg mt-1 font-bold">Background Changer</h3>
+                        <h3 class="text-lg mt-1 font-bold max-w-xs">Background Changer</h3>
                         <div className="flex justify-between items-center pt-3 pb-2">
                             <a href="#" className="px-4 py-2 bg-blue-600 hover:bg-blue-400 text-center text-sm text-white rounded duration-300">Live Demo</a>
                             <a className="text-black cursor-pointer text-3xl hover:text-blue-600" href='https://github.com/rukiyeaydin/rgb_color'>
@@ -102,10 +102,10 @@ const Forbeginnershero = () => {
                         </div>
                     </div>
                 </div>
-                <div className="p-3 w-auto bg-gray-100 rounded-sm">
-                    <div className="w-auto h-64"><img class="w-64 h-64 rounded-md" src={temperature}/></div>
+                <div className="p-3 w-auto bg-gray-100 rounded-2xl">
+                    <div className="w-auto h-80"><img class="w-80 h-80 rounded-3xl" src={temperature}/></div>
                     <div className="">
-                        <h3 class="text-lg mt-1 font-bold">Temperature Converter</h3>
+                        <h3 class="text-lg mt-1 font-bold max-w-xs">Temperature Converter</h3>
                         <div className="flex justify-between items-center pt-3 pb-2">
                             <a href="#" className="px-4 py-2 bg-blue-600 hover:bg-blue-400 text-center text-sm text-white rounded duration-300">Live Demo</a>
                             <a className="text-black cursor-pointer text-3xl hover:text-blue-600" href='https://github.com/rukiyeaydin/temperature_converter'>
